9a2451561f7c8971288306a92398ec9b674c39f1
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-01-26 14:38:05 -0500

    fix port

--- start-client.ps1
+++ start-client.ps1
@@ -2,5 +2,5 @@ $PATH_TO_ZROK="C:\work\git\bitbucket\dovholuk\dev_stuff\helper-scripts\windows\z
 $PRIVATE_ACCESS_TOKEN = Read-Host "Enter the private access token"
 
 Start-Process -FilePath "$PATH_TO_ZROK" `
-    -ArgumentList "access private $PRIVATE_ACCESS_TOKEN --bind 127.0.0.1:25564" `
+    -ArgumentList "access private $PRIVATE_ACCESS_TOKEN --bind 127.0.0.1:25565" `
     -PassThru
