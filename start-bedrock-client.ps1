f9a185dbbd9e70c308bfe7c7cf0ef0aa085c75fb
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-04-28 13:39:41 -0400

    run the LoopbackExempt for the user

--- start-bedrock-client.ps1
+++ start-bedrock-client.ps1
@@ -1,4 +1,6 @@
 $PATH_TO_ZROK="C:\minecraft\zrok.exe"
+CheckNetIsolation.exe LoopbackExempt -a -p=S-1-15-2-1958404141-86561845-1752920682-3514627264-368642714-62675701-733520436
+
 do {
     if (Test-Path $PATH_TO_ZROK -PathType Leaf) {
         break
